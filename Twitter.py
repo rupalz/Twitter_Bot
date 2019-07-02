2c7be98666d37324e3aae5eb8bf115835d8af324
rupalz <50055393+rupalz@users.noreply.github.com>
2019-07-02 16:49:11 +0530

    adding the main python file

---
+++ Twitter.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, print_function
+import tweepy
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_token_secret=""
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+api.update_status(status=input("what would you like to tweet -"))
